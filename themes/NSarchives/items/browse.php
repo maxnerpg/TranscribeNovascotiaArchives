cfe8b31867dc539713c0c1d9e45878c2f0e39766
Domenic Rosati <domenicrosati@gmail.com>
2016-11-11 15:15:56 -0400

    fix item browse grid

--- themes/NSarchives/items/browse.php
+++ themes/NSarchives/items/browse.php
@@ -13,27 +13,11 @@
             $sortLinks[__('Title')] = 'Dublin Core,Title';
             $sortLinks[__('Creator')] = 'Dublin Core,Creator';
             ?>
-            <div class="browse-items-header hidden-xs">
-                <div class="row">
-                    <div class="col-sm-3 col-sm-offset-2 col-md-2 col-md-offset-2">
-                        <?php echo browse_sort_links(array('Title'=>'Dublin Core,Title'), array('')); ?>
-                    </div>
-                    <div class="col-sm-3 col-md-2">
-                        <?php echo browse_sort_links(array('Creator'=>'Dublin Core,Creator'), array('')); ?>
-                    </div>
-                    <div class="hidden-sm col-md-2">
-                        Subject
-                    </div>
-                    <div class="col-sm-4 col-md-4">
-                        Description
-                    </div>
-                </div>
-            </div>
+        
         
             <?php foreach (loop('items') as $item): ?>
-            <div class="item">
-                <div class="row">
-                    <div class="col-sm-2 col-md-2">
+           
+                    <div class="col-sm-3">
                         <?php $image = $item->Files; ?>
                         <?php if ($image) {
                                 echo link_to_item('<div style="background-image: url(' . file_display_url($image[0], 'original') . ');" class="img"></div>');
@@ -41,23 +25,18 @@
                                 echo link_to_item('<div style="background-image: url(' . img('defaultImage@2x.jpg') . ');" class="img"></div>');
                             }
                         ?>
-                    </div>
-                    <div class="col-sm-3 col-md-2">
+                  
                         <?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?>
-                    </div>
-                    <div class="col-sm-3 col-md-2">
+                    
                         <?php echo metadata('item', array('Dublin Core', 'Creator')); ?>
-                    </div>
-                    <div class="hidden-sm col-md-2">
+                    
                         <?php echo metadata('item', array('Dublin Core', 'Subject')); ?>
-                    </div>
-                    <div class="col-sm-4 col-md-4">
+                   
                         <?php echo metadata('item', array('Dublin Core', 'Description'), array('snippet'=>150)); ?>
                     </div>
                 
                     <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
-                </div>
-            </div>
+            
             <?php endforeach; ?>
             <div id="outputs">
                 <span class="outputs-label"><?php echo __('Output Formats'); ?></span>
