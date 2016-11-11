3e305d86794d84d627ddd866958f7042e430a192
Domenic Rosati <domenicrosati@gmail.com>
2016-11-11 14:54:08 -0400

    browse grid view

--- themes/NSarchives/collections/browse.php
+++ themes/NSarchives/collections/browse.php
@@ -1,44 +1,41 @@
 <?php
-$pageTitle = __('Browse Collections');
-echo head(array('title'=>$pageTitle,'bodyclass' => 'collections browse'));
+    $pageTitle = __('Browse collections');
+    echo head(array('title'=>$pageTitle,'bodyclass' => 'collections browse'));
 ?>
-<div id="primary">
-<h1><?php echo $pageTitle; ?></h1>
-<?php echo pagination_links(); ?>
 
-<?php foreach (loop('collections') as $collection): ?>
-
-<div class="collection">
-
-    <h2><?php echo link_to_collection(); ?></h2>
-
-    <?php if (metadata('collection', array('Dublin Core', 'Description'))): ?>
-    <div class="element">
-        <h3><?php echo __('Description'); ?></h3>
-        <div class="element-text"><?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet'=>150))); ?></div>
+    <h1><?php echo 'Browse all collections'; ?></h1>
+    
+    <div class="browse-collections">
+        <?php if ($total_results > 0): ?>
+          
+            <?php foreach (loop('collections') as $collection): ?>
+                <div class="collection">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <?php if ($collectionImage = record_image('collection', 'square_thumbnail')): ?>
+                                <?php echo link_to_collection($collectionImage, array('class' => 'image')); ?>
+                            <?php endif; ?>
+                
+                            <?php echo link_to_collection(); ?>
+                    
+                            <?php if ($collection->hasContributor()): ?>
+                                <?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('all'=>true, 'delimiter'=>', ')); ?>
+                            <?php endif; ?>
+                    
+                            <?php if (metadata('collection', array('Dublin Core', 'Description'))): ?>
+                                <?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet'=>150))); ?>
+                            <?php endif; ?>
+                        </div>
+                    
+                        <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'collection' => $collection)); ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php else : ?>
+            <p><?php echo 'No collections added, yet.'; ?></p>
+        <?php endif; ?>
     </div>
-    <?php endif; ?>
+    <?php echo pagination_links(); ?>        
 
-    <?php if ($collection->hasContributor()): ?>
-    <div class="element">
-        <h3><?php echo __('Contributors(s)'); ?></h3>
-        <div class="element-text">
-            <p><?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('all'=>true, 'delimiter'=>', ')); ?></p>
-        </div>
-    </div>
-    <?php endif; ?>
-
-    <p class="view-items-link"><?php echo link_to_items_browse(__('View the items in %s', metadata('collection', array('Dublin Core', 'Title'))), array('collection' => metadata('collection', 'id'))); ?></p>
-
-    <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
-
-</div><!-- end class="collection" -->
-
-<?php endforeach; ?>
-
-<?php echo pagination_links(); ?>
-
-<?php fire_plugin_hook('public_collections_browse', array('collections'=>$collections, 'view' => $this)); ?>
-
+<?php fire_plugin_hook('public_collections_browse', array('collections'=> $collections, 'view' => $this)); ?>
 <?php echo foot(); ?>
-</div><!-- end primary -->
