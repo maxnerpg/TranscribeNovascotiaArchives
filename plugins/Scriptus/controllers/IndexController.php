186941e727ebcddff7bdfa6434fdbd2749105a62
Domenic Rosati <domenicrosati@gmail.com>
2016-11-11 22:49:50 -0400

    fix dashboard view

--- plugins/Scriptus/controllers/IndexController.php
+++ plugins/Scriptus/controllers/IndexController.php
@@ -258,7 +258,7 @@ class Scriptus_IndexController extends Omeka_Controller_AbstractActionController
 
 
     //Get the most recent transcriptions from the database.  The view also makes a query to the Disqus API to get most recent comments
-    public function submissionstatsAction(){
+    public function dashboardAction(){
 
         $user = current_user();
         $db = get_db();
@@ -392,7 +392,7 @@ class Scriptus_IndexController extends Omeka_Controller_AbstractActionController
     }
 
     //Get the new transcription submissions in past months.
-    public function submissionstats2Action(){
+    public function submissionstatsAction(){
 
         $currentYear = date("Y");
         $currentMonth = date("M");
