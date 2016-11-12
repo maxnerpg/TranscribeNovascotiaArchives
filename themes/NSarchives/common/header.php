a94151ffc7b95af9834f53962a267bc54f3e374b
Domenic Rosati <domenicrosati@gmail.com>
2016-11-12 10:28:40 -0400

    falvicon to favicon

--- themes/NSarchives/common/header.php
+++ themes/NSarchives/common/header.php
@@ -8,7 +8,7 @@
         <meta name="description" content="<?php echo $description; ?>" />
     <?php endif; ?>
 	<meta name="application-name" content="Transcribe@NovaScotiaArchives"/>
-	<link rel="icon" href="<?php echo img('falvicons/falvicon.ico'); ?>"  />
+	<link rel="icon" href="<?php echo img('favicons/flavicon.ico'); ?>"  />
 	
 
     <!-- Will build the page <title> -->
