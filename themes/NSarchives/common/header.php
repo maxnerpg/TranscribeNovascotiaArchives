501f928d6281410d4a5a2c299a8eb6541034cd0e
Domenic Rosati <domenicrosati@gmail.com>
2016-11-13 22:06:32 -0400

    rwd fix

--- themes/NSarchives/common/header.php
+++ themes/NSarchives/common/header.php
@@ -47,13 +47,7 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <style>
-    .fa{
-        line-height: 3;
-    
-    font-size: 18px;
-    }
-    </style>
+  
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
@@ -101,7 +95,7 @@
     					
 
     <main id="content" role="main">
-    <div class="btn-group col-sm-2 col-sm-offset-10
+    <div class="btn-group col-xs-12 col-sm-2 col-sm-offset-10
            container social-media">
 
     					    
