ac99d17f3092097a6972de0eb4f43a36cd0a817c
Domenic Rosati <domenicrosati@gmail.com>
2016-11-20 13:53:51 -0400

    Zend request api

--- plugins/Scriptus/controllers/IndexController.php
+++ plugins/Scriptus/controllers/IndexController.php
@@ -1,5 +1,6 @@
 <?php
-
+  use Zend\Http\Client;
+  
 class Scriptus_IndexController extends Omeka_Controller_AbstractActionController
 {
         
@@ -63,27 +64,23 @@ class Scriptus_IndexController extends Omeka_Controller_AbstractActionController
         $request = new Zend_Controller_Request_Http();
         $transcription = $request->getPost('transcription'); 
         // Get cURL resource
-        $curl = curl_init();
-        // Set some options - we are passing in a useragent too here
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
-            CURLOPT_POST => 1,
-            CURLOPT_POSTFIELDS => array(
-                secret => '6LeXxAsUAAAAABRtoOVXIUSOB0S9MkgoRbzJgVnx',
-                response => $request->getPost('g-recaptcha-response')
-         )
-        ));
-        // Send the request & save response to $resp
-        $resp = curl_exec($curl);
-        // Close request to clear up some resources
-        curl_close($curl);
-        return  $resp;
-        $captcha_response = json_decode($resp, TRUE);
-        if($captcha_response['success'] == false) {
-            return $resp;
-        }
-        
+      
+
+$client = new Client();
+$client->setUri('https://www.google.com/recaptcha/api/siteverify');
+$client->setMethod('POST');
+$client->setParameterPost(array(
+    secret => '6LeXxAsUAAAAABRtoOVXIUSOB0S9MkgoRbzJgVnx',
+    response => $request->getPost('g-recaptcha-response')
+));
+
+$response = $client->send();
+
+if ($response->isSuccess()) {
+    // the POST was successful
+}
+        return $response
+
         if (!$request->isPost()){
             throw new Exception('Request must be POST.');
         }
