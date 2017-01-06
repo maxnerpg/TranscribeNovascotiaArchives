8ce5294bfbd22d25f481980cfea815f0282abf84
Domenic Rosati <domenicrosati@gmail.com>
2017-01-06 18:46:44 -0400

    fix transcription window

--- themes/NSarchives/scriptus/index/transcribe.php
+++ themes/NSarchives/scriptus/index/transcribe.php
@@ -260,7 +260,7 @@ echo head(array('bodyid'=>'trancription','bodyclass'=>$collectionclass)); ?>
 
 				$('#ImageID').smoothZoom({
 					width: '100%',
-					height: '100%',
+					height: ($(window).height()),
 					responsive: true,
 					zoom_MAX: 200
 				});
