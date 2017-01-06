68aaa2768377bac377b20efccb9641d547e1bd6f
Domenic Rosati <domenicrosati@gmail.com>
2017-01-06 18:23:56 -0400

    add pencil edit to dashboard table

--- themes/NSarchives/scriptus/index/dashboard.php
+++ themes/NSarchives/scriptus/index/dashboard.php
@@ -48,7 +48,10 @@
     </div>
   </div>
 </div>
+<div class="container col-sm-12">
+ <div class="panel">
 <h3>Transcriptions</h3>
+</div>
 </div>
 <table id="Transcriptions" class="table table-striped table-bordered" cellspacing="0" width="100%">
  <thead>
@@ -56,7 +59,6 @@
                 <th>Item</th>
                 <th>Collection</th>
                 <th>Snippet</th>
-				<th></th>
                 <th>Last Updated</th>
             </tr>
         </thead>
@@ -65,8 +67,7 @@
 	<tr>
 		<td><?php echo $transcriptionItem["item_title"] . ', ' . $transcriptionItem["file_title"]; ?></td>
 		<td><a hred="<?php echo $transcribeItem["collection_link"];?>"><?php echo $transcriptionItem["collection_name"]; ?></a></td>
-		<td><?php echo snippet($transcriptionItem["transcription"], strlen($transcriptionItem["transcription"]) - 100, strlen($transcriptionItem["transcription"]))?></td>
-		<td><a href="<?php echo $transcriptionItem["URL_changed"];?>"><div class="hoverEdit"><span class="glyphicon glyphicon-pencil"></span></div></a></td>
+		<td><a href="<?php echo $transcriptionItem["URL_changed"];?>">...<?php echo snippet($transcriptionItem["transcription"], strlen($transcriptionItem["transcription"]) - 100, strlen($transcriptionItem["transcription"]))?><span class="glyphicon glyphicon-pencil"></span></a></td>
 		<td><?php 
 	                   $changedtime = new DateTime($transcriptionItem["time_changed"]);
 	                   $formattedchangedtime = $changedtime->format('F j, Y — g:s a');
@@ -76,6 +77,6 @@
 	<?php endforeach; ?>
 </tbody>
 </table>
-
+</div>
 
 </html>
