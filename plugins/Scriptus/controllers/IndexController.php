aa4153c4a8b099a207494a2a7327c2e7bc788ce8
Domenic Rosati <domenicrosati@gmail.com>
2017-01-09 18:31:57 -0400

    add verify routes

--- plugins/Scriptus/controllers/IndexController.php
+++ plugins/Scriptus/controllers/IndexController.php
@@ -200,7 +200,16 @@ class Scriptus_IndexController extends Omeka_Controller_AbstractActionController
 
     public function verifyAction() 
      {        
-
+         $secret="6LeXxAsUAAAAABRtoOVXIUSOB0S9MkgoRbzJgVnx";
+$response=$_POST["captcha"];
+$verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
+$captcha_success=json_decode($verify);
+if ($captcha_success->success==false) {
+ echo 'success';
+}
+else if ($captcha_success->success==true) {
+  echo 'faliure';
+}
       
        
     }
