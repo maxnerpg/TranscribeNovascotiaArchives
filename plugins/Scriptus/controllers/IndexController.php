fd40df861a250f7ccfa131f3b1943cb325d4299c
Domenic Rosati <domenicrosati@gmail.com>
2017-01-09 19:02:30 -0400

    verification tests added

--- plugins/Scriptus/controllers/IndexController.php
+++ plugins/Scriptus/controllers/IndexController.php
@@ -205,10 +205,10 @@ $response=$_POST["captcha"];
 $verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
 $captcha_success=json_decode($verify);
 if ($captcha_success->success==false) {
- echo 'success';
+ echo $captcha_success;
 }
 else if ($captcha_success->success==true) {
-  echo 'faliure';
+  echo $captcha_success;
 }
       
        
