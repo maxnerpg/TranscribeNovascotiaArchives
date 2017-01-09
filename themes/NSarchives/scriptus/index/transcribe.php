82526fa245c7f4bd75f12a5bd4778ec782dcc62e
Domenic Rosati <domenicrosati@gmail.com>
2017-01-09 18:33:48 -0400

    add verify routes

--- themes/NSarchives/scriptus/index/transcribe.php
+++ themes/NSarchives/scriptus/index/transcribe.php
@@ -261,7 +261,7 @@ echo $this->fileOrder;
 				$("#transcribebox").prop("readonly", false);
 				$.ajax({
 					type: "POST",
-					url: "../../../verify.php",
+					url: "../../../verify",
 					data: {
 						captcha: grecaptcha.getResponse()
 					}
