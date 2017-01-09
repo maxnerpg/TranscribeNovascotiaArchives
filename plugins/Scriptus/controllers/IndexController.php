5f3e376cdc12128543ceb11e261f473f97350576
Domenic Rosati <domenicrosati@gmail.com>
2017-01-09 19:05:28 -0400

    captcha string conversion

--- plugins/Scriptus/controllers/IndexController.php
+++ plugins/Scriptus/controllers/IndexController.php
@@ -205,10 +205,10 @@ $response=$_POST["captcha"];
 $verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
 $captcha_success=json_decode($verify);
 if ($captcha_success->success==false) {
- echo $captcha_success;
+ echo var_dump($captcha_success);
 }
 else if ($captcha_success->success==true) {
-  echo $captcha_success;
+  echo var_dump($captcha_success);
 }
       
        
