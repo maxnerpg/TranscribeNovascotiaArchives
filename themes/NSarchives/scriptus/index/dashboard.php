c0c14d59b0727a11ab9f4d19503f2cfa21cd0e93
Domenic Rosati <domenicrosati@gmail.com>
2017-01-14 20:34:25 -0400

    test

--- themes/NSarchives/scriptus/index/dashboard.php
+++ themes/NSarchives/scriptus/index/dashboard.php
@@ -1,1 +1,37 @@
-<h1> tes </h1>
+
+<?php echo head(array('title'=> 'Dashboard')); ?>
+
+<style>
+@media (max-width: 767px) {
+	.main {
+	padding-left: 0;
+	}
+	div.collectionTitle {
+	width:100%;
+
+	padding-left:15px;
+	}
+	.collectionTitle h1 {
+	text-align:left;
+	}
+	div.collectionDesc {
+		margin-top:0;
+		padding-right:10px;
+
+	}
+	div#pageshavebeen {
+		text-align:left;
+	}
+}
+</style>
+<div class="container">
+ 
+</div>
+<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('#Transcriptions').DataTable();
+} );
+</script>
+</html>
